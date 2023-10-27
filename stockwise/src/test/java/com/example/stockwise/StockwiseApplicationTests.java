7f2279266fd72342488655e88f1fad922d47c275
Vidhi Agarwal <avidhi2108@gmail.com>
2023-10-27 12:29:09 -0700

    Added a spring boot module

---
+++ stockwise/src/test/java/com/example/stockwise/StockwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockwiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
