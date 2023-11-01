3ad1a7425fddf55056ccef019a8a10fd25b82808
aishwarya <aishwarya.manoharan@sjsu.edu>
2023-10-31 20:39:57 -0700

    Updated the vendor.jsx

--- frontend/stockwise-frontend/src/pages/vendor/Vendor.jsx
+++ frontend/stockwise-frontend/src/pages/vendor/Vendor.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function VendorList() {
+function Vendor() {
   const [vendors, setVendors] = useState([]);
 
   useEffect(() => {
-    fetch('/vendors/')
+    axios.get('/vendors')
       .then(response => {
-        if (!response.ok) {
+        if (!response.data.success) {
           throw new Error('Network response was not ok');
         }
-        return response.json();
+        return response.data.vendors;
       })
       .then(data => {
         setVendors(data);
@@ -20,28 +21,26 @@ function VendorList() {
   }, []);
 
   return (
-      <div>
-        <h1>Vendor List</h1>
-        <table>
+    <div>
+      <h1>Vendor List</h1>
+      <table>
+        <tr>
+          <th>Vendor Name</th>
+          <th>Company Name</th>
+          <th>Email</th>
+          <th>Work Phone</th>
+        </tr>
+        {vendors.map(vendor => (
+          <tr key={vendor.id}>
+            <td>{vendor.vendorName}</td>
+            <td>{vendor.companyName}</td>
+            <td>{vendor.email}</td>
+            <td>{vendor.mobileNumber}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
 
-            <tr>
-              <th>Vendor Name</th>
-              <th>Company Name</th>
-              <th>Email</th>
-              <th>Work Phone</th>
-            </tr>
-            {vendors.map(vendor => (
-              <tr key={vendor.id}>
-                <td>{vendor.vendorName}</td>
-                <td>{vendor.companyName}</td>
-                <td>{vendor.email}</td>
-                <td>{vendor.mobileNumber}</td>
-              </tr>
-            ))}
-
-        </table>
-      </div>
-    );
-  }
-
-  export default VendorList;
+export default Vendor;
