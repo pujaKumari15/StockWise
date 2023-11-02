154a901a9215387bf12d346d15cc1ad0a893898d
aishwarya <aishwarya.manoharan@sjsu.edu>
2023-11-02 00:44:37 -0700

    updating purchase orders

--- frontend/stockwise-frontend/src/pages/purchaseorders/Purchaseorders.jsx
+++ frontend/stockwise-frontend/src/pages/purchaseorders/Purchaseorders.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Sidebar from "../../components/sidebar/Sidebar";
+import './Purchaseorders.css';
 
 function PurchaseOrders() {
-  const [purchaseOrders, setPurchaseOrders] = useState([]);
+  const [purchaseOrders, setPurchaseOrders] = useState([
+  { "id": 123, "mobileNumber": 12345678, "vendorName": "Test Vendor Name 1", "lastPurchaseDate": '2023-10-28'}]);
+
 
   useEffect(() => {
     axios.get('/purchase_orders')
@@ -21,16 +25,30 @@ function PurchaseOrders() {
   }, []);
 
   return (
-    <div>
+  <>
+      <Sidebar/>
       <h1>Purchase Orders</h1>
-      <table>
+      <div className="orderList">
+      <table className="orderTable">
+      <thead>
           <tr>
             <th>Vendor Name</th>
             <th>Vendor Phone Number</th>
             <th>Last Purchased Date</th>
           </tr>
+          </thead>
+        <tbody>
+        {purchaseOrders.map(order => (
+          <tr key={order.id}>
+            <td>{order.vendorName}</td>
+            <td>{order.mobileNumber}</td>
+            <td>{order.lastPurchaseDate}</td>
+          </tr>
+        ))}
+        </tbody>
       </table>
-    </div>
+      </div>
+    </>
   );
 }
 
