36f0c71d9e83a0f2a3b8d1d99ed0311bd1200fd9
pujakumari <puja.kumari@sjsu.edu>
2023-12-01 11:06:04 -0800

    Updated dashboard

--- frontend/stockwise-frontend/src/pages/dashboard/MainContent.jsx
+++ frontend/stockwise-frontend/src/pages/dashboard/MainContent.jsx
@@ -3,7 +3,7 @@ import
 { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill}
     from 'react-icons/bs'
 import
-{ BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line }
+{ BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, PieChart, Pie, LineChart,Line }
     from 'recharts';
 
 function MainContent() {
@@ -11,47 +11,73 @@ function MainContent() {
     const data = [
         {
             name: 'Page A',
-            uv: 4000,
             pv: 2400,
             amt: 2400,
         },
         {
             name: 'Page B',
-            uv: 3000,
             pv: 1398,
             amt: 2210,
         },
         {
             name: 'Page C',
-            uv: 2000,
             pv: 9800,
             amt: 2290,
         },
         {
             name: 'Page D',
-            uv: 2780,
             pv: 3908,
             amt: 2000,
         },
         {
             name: 'Page E',
-            uv: 1890,
             pv: 4800,
             amt: 2181,
         },
         {
             name: 'Page F',
-            uv: 2390,
             pv: 3800,
             amt: 2500,
         },
         {
             name: 'Page G',
-            uv: 3490,
             pv: 4300,
             amt: 2100,
         },
     ];
+
+    const salesData = [
+        { month: 'Jan', sales: 2000 },
+        { month: 'Feb', sales: 3000 },
+        { month: 'Mar', sales: 4000 },
+        { month: 'Apr', sales: 2800 },
+        { month: 'May', sales: 1400 },
+        { month: 'Jun', sales: 2200 },
+        { month: 'Jul', sales: 3200 },
+        { month: 'Aug', sales: 5000 },
+        { month: 'Sept', sales: 2400 },
+        { month: 'Oct', sales: 3000 },
+        { month: 'Nov', sales: 4500 },
+        { month: 'Dec', sales: 3500 },
+      ];
+
+      const salesByCategories = [
+        { category: 'Dairy', sales: 3000 },
+        { category: 'Vegetables', sales: 3200 },
+        { category: 'Staples', sales: 4000 },
+        { category: 'Fruits', sales: 2800 },
+        { category: 'Bakery', sales: 3500 },
+       {/*} { category: 'Beverages', sales: 2000 },
+        { category: 'Meat & Seafood', sales: 2000 },
+        { category: 'Frozen Food', sales: 2000 },
+        { category: 'Condiments', sales: 2000 },
+        { category: 'Cleaning & Household', sales: 2000 },
+        { category: 'Personal Hygiene', sales: 2000 },
+      { category: 'Cooking Essentials', sales: 2000 } */}
+      ];
+
+      const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#008042'];
+
 
 
     return (
@@ -92,11 +118,11 @@ function MainContent() {
             </div>
 
             <div className='charts'>
-                <ResponsiveContainer width="100%" height="100%">
+            <ResponsiveContainer width="100%" height="100%">
                     <BarChart
                         width={500}
                         height={300}
-                        data={data}
+                        data={salesData}
                         margin={{
                             top: 5,
                             right: 30,
@@ -105,16 +131,37 @@ function MainContent() {
                         }}
                     >
                         <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
+                        <XAxis dataKey="month" />
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Bar dataKey="pv" fill="#8884d8" />
-                        <Bar dataKey="uv" fill="#82ca9d" />
+                        <Bar dataKey="sales" fill="#8884d8" />
                     </BarChart>
                 </ResponsiveContainer>
 
-                <ResponsiveContainer width="100%" height="100%">
+                <ResponsiveContainer>
+                <PieChart width={200} height={100}>
+      <Pie
+        data={salesByCategories}
+        dataKey="sales"
+        nameKey="category"
+        cx="50%"
+        cy="50%"
+        outerRadius={110}
+        fill="#8884d8"
+        label
+      >
+        {salesData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip />
+      <Legend />
+    </PieChart>
+                </ResponsiveContainer>
+
+
+            {/*}    <ResponsiveContainer width="100%" height="100%">
                     <LineChart
                         width={500}
                         height={300}
@@ -134,7 +181,15 @@ function MainContent() {
                         <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
                         <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
                     </LineChart>
-                </ResponsiveContainer>
+                </ResponsiveContainer> 8/}
+
+              {/*  <ResponsiveContainer width="100%" height="100%">
+                <PieChart width={500} height={300}>
+                <Pie data={salesByCategories} dataKey="sales" nameKey="category" cx="50%" cy="50%" outerRadius={150} fill="#8884d8" label />
+                <Tooltip />
+                <Legend />
+                </PieChart>
+    </ResponsiveContainer> */}
 
             </div>
         </main>
